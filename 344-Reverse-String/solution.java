8c52f4e73cc3944ded5f8e3deaadf6be82c9ac6e
ZachZen <fyfyzzk@hotmail.com>
2016-06-13 21:14:13 -0400

    [344. Reverse String][Compile Error]committed by LeetCode Extension

---
+++ 344-Reverse-String/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public String reverseString(String s) {
+        int i = 0;
+        int len = 0;
+        char tmp = "";
+        
+        if (null == s) {
+            return s;
+        }
+        
+        len = s.length;
+        
+        for (i = 0; i < len; i) {
+            tmp = s[i];
+            s[len - 1 - i] = s[i];
+            s[i] = tmp;
+        }
+        
+        return s;
+    }
+}
