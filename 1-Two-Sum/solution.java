c4d6a1a7a506003216074d2c08ac20917b784b9a
ZachZen <fyfyzzk@hotmail.com>
2016-07-31 14:53:20 -0400

    [1. Two Sum][Accepted]committed by LeetCode Extension

---
+++ 1-Two-Sum/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = {-1, -1};
+        int secondValue = 0;
+
+        if (0 == nums.length) {
+            return result;
+        }
+        
+        //  pick the first value
+        for (int i = 0; i < nums.length; i++) {
+            secondValue = target - nums[i];
+            
+            //  pick the second value
+            for (int j = i + 1; j < nums.length; j++) {
+                if (secondValue == nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                    
+                    return result;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
