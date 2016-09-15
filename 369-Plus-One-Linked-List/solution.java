515a33616459b50f553259dbc2084a86dfd7a772
ZachZen <fyfyzzk@hotmail.com>
2016-09-15 19:49:50 -0400

    [369. Plus One Linked List][Accepted]committed by LeetCode Extension

--- 369-Plus-One-Linked-List/solution.java
+++ 369-Plus-One-Linked-List/solution.java
@@ -15,29 +15,44 @@
 public class Solution {
     private int helper(ListNode root, int value) {
         int tmp = 0;
+        int ret = 0;
         
-        if (root.next == null) {
-            root.val = (root.val + value) % 10;
-            return (root.val + value) > 10 ? 1 : 0;
+        if (root == null) {
+            return value;
         }
         
         tmp = helper(root.next, value);
-        root.val += tmp % 10;
         
-        return (tmp + root.val) > 10 ? 1 : 0;
+        ret = (tmp + root.val) >= 10 ? 1 : 0;
+        
+        System.out.println("tmp" + tmp + "return " + ((tmp + root.val) >= 10 ? 1 : 0));
+        
+        root.val = (root.val + tmp) % 10; 
+        
+        return ret;
     }
     
     public ListNode plusOne(ListNode head) {
         if (null == head) {
             return head;
         }
-        ListNode dummy = new ListNode();
+        
+        ListNode dummy = new ListNode(0);
         dummy.next = head;
         
         if (1 == helper(head, 1)) {
             dummy.next = new ListNode(1);
             dummy.next.next = head;
         }
+        
+		ListNode t = dummy.next;
+		
+		//System.out.println(t.val);
+		
+		while (t != null) {
+			System.out.println(t.val);
+			t = t.next;
+		}
         
         return dummy.next;
     }
