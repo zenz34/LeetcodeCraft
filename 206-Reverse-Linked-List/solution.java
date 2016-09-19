2a67dedea1513232098b84b46e8d0b6a25878f6f
ZachZen <fyfyzzk@hotmail.com>
2016-09-19 19:38:45 -0400

    [206. Reverse Linked List][Accepted]committed by LeetCode Extension

--- 206-Reverse-Linked-List/solution.java
+++ 206-Reverse-Linked-List/solution.java
@@ -8,24 +8,7 @@
  */
 public class Solution {
     public ListNode reverseList(ListNode head) {
-        /*
-        if (head == null || head.next == null) {
-            return head;
-        }
-        */
         ListNode pre = null;
-        
-        /*
-        ListNode cur = head;
-        
-        while (cur != null) {
-            head = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = head;
-        }
-        */
-        
         ListNode next = head;
         
         while (head != null) {
