5f8832a406b08f4b69d2da5573d82abde5511756
ZachZen <fyfyzzk@hotmail.com>
2016-09-19 20:24:52 -0400

    [92. Reverse Linked List II][Compile Error]committed by LeetCode Extension

---
+++ 92-Reverse-Linked-List-II/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = ListNode(0);
+        dummy.next = head;
+        ListNode startPre = dummy;
+        ListNode endPre = dummy;
+        ListNode startCur = null;
+        ListNode endCur = null;
+        ListNode tmp = null;
+        
+        m--;
+        n--;
+        
+        while (m-- > 0) {
+            startPre = startPre.next;
+        }
+        
+        //startCur = startPre.next;
+        
+        while (n-- > 0) {
+            endPre = endPre.next;
+        }
+        
+        //endCur = endPre.next;
+        head = endPre;
+        
+        while (startPre != head) {
+            startCur = startPre.next;
+            endCur = endPre.next;
+            
+            startPre.next = endCur;
+            tmp = startCur.next;
+            startCur.next = endCur.next;
+            
+            endPre.next = startCur;
+            endCur.next = tmp;
+
+            startPre = startPre.next;
+            endPre = endPre.next;
+        }
+        
+        return dummy.next;
+    }
+}
