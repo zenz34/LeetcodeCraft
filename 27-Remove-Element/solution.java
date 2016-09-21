bc63905676729455225954632f563aa83a646f08
ZachZen <fyfyzzk@hotmail.com>
2016-09-21 11:34:05 -0400

    [27. Remove Element][Compile Error]committed by LeetCode Extension

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int startIndex = 0;
         int endIndex = 0;
@@ -12,19 +12,24 @@ public class Solution {
         startIndex = 0;
         endIndex = nums.length - 1;
 
-        for (; startIndex != endIndex; endIndex--) {  //  startIndex != endIndex   length == 0 optm
-            if (nums[endIndex] == val) {
-                
-                while (nums[startIndex] != val) {
-                    startIndex++;
+        for (; startIndex < endIndex; startIndex++) {  //  startIndex != endIndex   length == 0 optm
+            if (nums[startIndex] == val) {
+
+                while (nums[endIndex] == val) {
+                    endIndex--;
+
+                    if (startIndex >= endIndex) {
+                        break;
+                    }
                 }
-                
-                nums[endIndex] = nums[startIndex];
-                nums[startIndex] = val;
-                startIndex++;
+
+                nums[startIndex] = nums[endIndex];
+                nums[endIndex] = val;
+                endIndex--;
             }
         }
-        
+
+
         //nums = nums + startIndex;
         newLength = nums.length - startIndex - 1;
 
