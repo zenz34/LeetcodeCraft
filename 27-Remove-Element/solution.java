c537025d502e1a22c69b5f94830eb9480c6b522f
ZachZen <fyfyzzk@hotmail.com>
2016-09-22 20:17:48 -0400

    [27. Remove Element][Accepted]committed by LeetCode Extension

--- 27-Remove-Element/solution.java
+++ 27-Remove-Element/solution.java
@@ -1,53 +1,15 @@
 public class Solution {
-    public int removeElement(int[] nums, int val) {
-        int startIndex = 0;
-        int endIndex = 0;
-        int newLength = 0;
-        boolean flag = false;
+    public int removeElement(int[] A, int elem) {
+       int m = 0;
+       for(int i = 0; i < A.length; i++){
 
-        //  corner case
-        if (nums == null || nums.length == 0) { //length == 0?
-            return startIndex;
-        }
+           if(A[i] != elem){
+               A[m] = A[i];
 
-        //startIndex = 0;
-        endIndex = nums.length - 1;
+               m++;
+           }
+       }
 
-        //  for [3, 3] target 3
-        
-        for (; startIndex < endIndex; startIndex++) {  //  startIndex != endIndex   length == 0 optm
-            if (nums[startIndex] == val) {
-
-                //  find end index not equals to target
-                while (nums[endIndex] == val) {
-                    endIndex--;
-
-                    //  end condition
-                    if (startIndex >= endIndex) {
-                        flag = true;
-                        break;
-                    }
-                }
-
-                if (true == flag) {
-                    break;
-                }
-
-                nums[startIndex] = nums[endIndex];
-                nums[endIndex] = val;
-                endIndex--;
-            }
-        }
-
-        //if (startIndex == 0 && endIndex == nums.length - 1) {
-            //  for [2]  target = 3
-        if (nums[startIndex] != val) {
-            newLength = startIndex + 1;
-        }
-        else {
-            newLength = startIndex;
-        }
-
-        return newLength;
+       return m;
     }
 }
