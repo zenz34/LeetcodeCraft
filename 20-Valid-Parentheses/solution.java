c7985aa829808f9cd38f7e08989aa61a712ffb10
ZachZen <fyfyzzk@hotmail.com>
2016-10-03 17:57:14 -0400

    [20. Valid Parentheses][Compile Error]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,45 +1,45 @@
 public class Solution {
     public boolean isValid(String s) {
-        if (s == null || s.length() == 0) {
-            return true;
-        }
-        
-        if (s.length() % 2 != 0) {
+     if (str == null || str.length() % 2 == 1) {
             return false;
         }
-        
-        char preCh = '\0';
-        
-        for (int i = 0; i != s.length - 1; i++) {
-            if (i % 2 == 0) {   //  odd
-                if (s.charAt(i) != '(' && s.charAt(0) != '[' && s.charAt(0) != '{') {
-                    return false;
-                }
-                
-                preCh = s.charAt(i);
-            }
-            else {              //  even
-                if (preCh == '(') {
-                    if (s.charAt(i) != ')') {
+
+        Stack<Character> stack = new Stack<Character>();
+        char ch = '\0';
+
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+
+
+
+            switch (ch) {
+                case '{':
+                case '[':
+                case '(':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
                         return false;
                     }
-                }
-                else if (preCh == '[') {
-                    if (s.charAt(i) != ']') {
+                    break;
+                case '}':
+                    //System.out.println(stack.pop());
+                    if (stack.isEmpty() || stack.pop() != '{') {
                         return false;
-                    }                    
-                }
-                else if (preCh == '{') {
-                    if (s.charAt(i) != '}') {
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
                         return false;
-                    }                    
-                }
-                else {
-                    return false;
-                }
+                    }
+                    break;
+                default:
+                    //return false;
+                    break;
             }
         }
-        
-        return true;
+
+        return stack.isEmpty();
     }
 }
