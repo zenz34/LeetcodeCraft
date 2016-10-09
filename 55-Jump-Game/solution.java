a46065a503866b20a3e18c8870f229930c9940ab
ZachZen <fyfyzzk@hotmail.com>
2016-10-08 20:37:14 -0400

    [55. Jump Game][Runtime Error]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,30 +1,46 @@
 public class Solution {
-    private boolean helper(int[] nums, int curStep, int maxIncStep, int goalStep) {
-        if (curStep + maxIncStep >= goalStep) {
-            System.out.println("  helper return true");
-
-            return true;
-        }
-
-        for (int i = 1; i < maxIncStep; i++) {
-
-            System.out.println("  helper in for loop" + i + " curStep " + curStep + "maxIncStep " + maxIncStep);
-
-            if (helper(nums, curStep + i, nums[curStep + i], goalStep)) {
+        private boolean helper(int[] nums, int[] visited, int curStep, int goalStep) {
+            if (curStep == goalStep) {
+                System.out.println("  helper return true");
+            
                 return true;
             }
-        }
-
-        return false;
-    }
-
-    public boolean canJump(int[] nums) {
-        if (nums == null) {
+            
+            for (int i = 1; i <= nums[curStep]; i++) {
+            
+                System.out.println("  helper in for loop i = " + i + " curStep " + curStep + " visited[curStep] = " + visited[curStep]);
+            
+                if (visited[curStep + i] == 0) {
+                    if (helper(nums, visited, curStep + i, goalStep)) {
+                        return true;
+                    }
+                }
+            }
+            
+            System.out.println("  helper visited curStep " + curStep);
+            visited[curStep] = 1;
+            
             return false;
         }
 
-
-
-        return helper(nums, 0, nums[0], nums.length - 1);
-    }
+        public boolean canJump(int[] nums) {
+            if (nums == null) {
+                return false;
+            }
+    
+            int[] visited = new int[nums.length];
+    
+            for (int i = 0; i < nums.length; i++) {
+                visited[i] = 0;
+            }
+    
+    
+            boolean flag =  helper(nums, visited, 0, nums.length - 1);
+    
+            for (int i : visited) {
+                System.out.println("i  = " + i);
+            }
+    
+            return flag;
+        }
 }
