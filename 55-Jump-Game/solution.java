1e290eb47f6c6e8b887ff3b152f1f6cffe60ebbc
ZachZen <fyfyzzk@hotmail.com>
2016-10-08 20:38:09 -0400

    [55. Jump Game][Runtime Error]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,15 +1,10 @@
 public class Solution {
         private boolean helper(int[] nums, int[] visited, int curStep, int goalStep) {
             if (curStep == goalStep) {
-                System.out.println("  helper return true");
-            
                 return true;
             }
             
             for (int i = 1; i <= nums[curStep]; i++) {
-            
-                System.out.println("  helper in for loop i = " + i + " curStep " + curStep + " visited[curStep] = " + visited[curStep]);
-            
                 if (visited[curStep + i] == 0) {
                     if (helper(nums, visited, curStep + i, goalStep)) {
                         return true;
@@ -17,7 +12,6 @@ public class Solution {
                 }
             }
             
-            System.out.println("  helper visited curStep " + curStep);
             visited[curStep] = 1;
             
             return false;
@@ -36,10 +30,6 @@ public class Solution {
     
     
             boolean flag =  helper(nums, visited, 0, nums.length - 1);
-    
-            for (int i : visited) {
-                System.out.println("i  = " + i);
-            }
     
             return flag;
         }
